943dc00a329e2b1fb61fb111e98fcc84ece908ae
Angela Williams <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2020-01-20 16:44:19 -0700

    MVP completed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import * as rtl from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/react/cleanup-after-each';
 import Dashboard from './Dashboard';
-import { getByTestId } from '@testing-library/react';
-// import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 
-afterEach(rtl.cleanup);
-it('renders without crashing', () => {
+describe('<Dashboard />', () => {
+  it('should match snapshot', () => {
+    const tree = renderer.create(<Dashboard />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
 
-it('renders "display header', () => {
-    const wrapper = rtl.render();
-    const hasDisplayHeader = wrapper.queryByText(/this is the display component/i);
-});
+  describe('<Dashboard />', () => {
+    it('button should display "Close Gate" when clicked', () => {
+      const { getByText } = render(<Dashboard />);
+  
+      const closeGateBtn = getByText(/close gate/i)
+      fireEvent.click(closeGateBtn);
+      expect(getByText(/open gat/i)).toBeInTheDocument();
+      
+    });
+  });
+  
+  describe('<Dashboard />', () => {
+      it('closed should render when clicking close gate', () => {
+        const { getByText } = render(<Dashboard />);
+        const button = getByText(/close gate/i);
+        fireEvent.click(button);
+        const closedSign = getByText(/closed/i);
+        expect(closedSign).toBeInTheDocument();
+      });
+    });
